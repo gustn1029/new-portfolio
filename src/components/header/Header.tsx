c45471bfec6749f594c40777ab83d8f6e5936770
hs0820 <gustn1029@gmail.com>
2024-10-07 17:59:35 +0900

    헤더 컴포넌트 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="fixed left-[50%] translate-x-[-50%] top-0 inner z-[100]">
       <Link to={"/"}>
         <h1>
-          <em className="font-bold text-white text-[1.7rem]"><span className="text-[#3b82f6]">K</span>HS</em>
+          <em className="font-bold text-white text-[1.7rem]"><span className="text-[#eab308]">K</span>HS</em>
           <span className="inline-block indent-[-9999px]">portfolio</span>
         </h1>
       </Link>
