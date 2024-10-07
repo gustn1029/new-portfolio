9596857dd311ab0f172f3b69c12536ad6c3fbbeb
hs0820 <gustn1029@gmail.com>
2024-10-07 18:00:06 +0900

    폳더 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/header/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Main from "./components/pages/main/Main";
+import Main from "./pages/main/Main";
 
 function App() {
   return (
