8509320929b0637159a5f8eb1ecf738dead866f0
hs0820 <gustn1029@gmail.com>
2024-10-07 17:52:45 +0900

    tailwindcss 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maskImage: {
+        "fade-to-transparent":
+          "linear-gradient(to top, black 70%, transparent 100%)",
+      },
+    },
   },
   plugins: [],
-}
+};
