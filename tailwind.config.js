bc0466f37cc4217180ce981f42ad6d89f15ffb37
hs0820 <gustn1029@gmail.com>
2024-10-13 00:21:21 +0900

    색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#eab308",
+        point: "#FFCC74"
+      },
       maskImage: {
         "fade-to-transparent":
           "linear-gradient(to top, black 70%, transparent 100%)",
