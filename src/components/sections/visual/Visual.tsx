78fb529c1b23af50097c7794b871137772879984
hs0820 <gustn1029@gmail.com>
2024-10-13 00:21:10 +0900

    visual id 설정 및 색상 변경

--- src/components/sections/visual/Visual.tsx
+++ src/components/sections/visual/Visual.tsx
@@ -4,14 +4,21 @@ import styles from "./visual.module.css";
 const Visual = () => {
   return (
     <section
+      id="visual"
       className={`flex flex-col mb-[100px] justify-center items-center gap-[10%] w-full h-screen ${styles.visualWrap}`}
     >
-      <h2 className="s-only">포트폴리오 메인 소개</h2>
+      <h2 className="sr-only">포트폴리오 메인 소개</h2>
       <div className="text-center text-white text-[3rem] font-bold">
-        <p className="uppercase">I am <em className="text-[#eab308]">KimHyeonsoo</em></p>
-        <p className="uppercase"><span className="text-[#eab308]">Your</span> next frontend developer</p>
+        <p className="uppercase">
+          I am <em className="text-point">KimHyeonsoo</em>
+        </p>
+        <p className="uppercase">
+          <span className="text-point">Your next</span> frontend developer
+        </p>
       </div>
-      <LinkButton href="#Project" buttonClassName="">Previous Projects</LinkButton>
+      <LinkButton href="#Project" buttonClassName="">
+        Previous Projects
+      </LinkButton>
     </section>
   );
 };
