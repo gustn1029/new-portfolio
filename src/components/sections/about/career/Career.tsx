679acaefbfd9b1701969f6d4ed6c94c4257f880b
hs0820 <gustn1029@gmail.com>
2024-10-13 00:22:07 +0900

    career 컴포넌트 추가

---
+++ src/components/sections/about/career/Career.tsx
@@ -0,0 +1,48 @@
+import { Fragment } from "react";
+import CareerListItem from "./CareerListItem";
+
+export interface CareerItem {
+  period: string;
+  company?: string;
+  education?: string;
+  desc: string[];
+}
+
+export interface CareerList {
+  career: string;
+  careerList: CareerItem[];
+}
+
+interface CareerProps {
+  careerData: CareerList[];
+}
+
+const Career = ({ careerData }: CareerProps) => {
+  return (
+    <div className="flex-grow">
+      <h3 className="sr-only">career</h3>
+      <section className="flex items-start justify-between">
+        {careerData.map((data) => (
+          <Fragment key={data.career}>
+            <dl className="w-[calc(50%-30px)]">
+            <dt className={`pb-[5px] text-white text-[1.3rem] font-bold border-b border-b-gray-500 `}>{data.career}</dt>
+            {data.careerList.map((career) => {
+              return (
+                <CareerListItem
+                  key={`${data.career}_${career.company || career.education}`}
+                  period={career.period}
+                  company={career.company}
+                  education={career.education}
+                  desc={career.desc}
+                />
+              );
+            })}
+            </dl>
+          </Fragment>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default Career;
---
+++ src/components/sections/about/career/CareerListItem.tsx
@@ -0,0 +1,26 @@
+import { CareerItem } from "./Career";
+
+const CareerListItem = ({ period, company, education, desc }: CareerItem) => {
+  return (
+    <dd className="mt-[20px]">
+      <div>
+        <h4 className="inline-block text-point mr-[20px] mb-[5px]">
+          {company ? company : education}
+        </h4>
+        <span className="text-gray-600">{period}</span>
+      </div>
+      <ul>
+        {desc.map((text, idx) => (
+          <li
+            key={`${company || education}_${idx}`}
+            className={`text-gray-400 break-keep ${idx > 0 ? "mt-[3px]":""}`}
+          >
+            {text}
+          </li>
+        ))}
+      </ul>
+    </dd>
+  );
+};
+
+export default CareerListItem;
