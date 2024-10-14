ef15cea4340f194f7b3c372a22b9e9d513b23c4e
hs0820 <gustn1029@gmail.com>
2024-10-14 23:21:28 +0900

    title component desc 추가

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,15 +1,19 @@
 import styles from "./title.module.css";
 
 interface TitleProps {
-    title: string;
+  title: string;
+  desc?:string;
 }
-const Title = ({title}:TitleProps) => {
+const Title = ({ title, desc }: TitleProps) => {
   return (
-    <h2
-      className={`flex mb-[50px] items-end gap-[10px] leading-[1] uppercase text-[2rem] font-bold text-yellow-500 ${styles.title}`}
-    >
-      <span className="relative">{title}</span>
-    </h2>
+    <>
+      <h2
+        className={`flex ${desc ? "mb-[10px]":"mb-[50px]"} items-end gap-[10px] leading-[1] uppercase text-[2rem] font-bold text-yellow-500 ${styles.title}`}
+      >
+        <span className="relative">{title}</span>
+      </h2>
+      {desc && <p className="mb-[50px] text-gray-500 text-[0.9rem]">{desc}</p>}
+    </>
   );
 };
 
