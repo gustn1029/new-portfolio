c8414b4affe4e0854105aea7beea54aaf0e23c5f
hs0820 <gustn1029@gmail.com>
2024-10-24 01:18:43 +0900

    이메일 전송 기능 추가

--- src/components/sections/contact/Contact.tsx
+++ src/components/sections/contact/Contact.tsx
@@ -1,34 +1,43 @@
-import { ChangeEvent, FormEvent, useRef } from "react";
+import { ChangeEvent, useRef } from "react";
 import SectionWrap from "../SectionWrap";
 import { useMutation } from "@tanstack/react-query";
 import { FetchContact } from "../../../utils/http";
 import toast from "react-hot-toast";
 import { useForm } from "react-hook-form";
 import { ContactMailData } from "../../../types";
-import ErrorToolTip from "../../tooltip/ErrorToolTip";
 import LabelLayout from "../../inputs/LabelLayout";
 import {
   ButtonStyleEnum,
   ButtonStyleList,
 } from "../../../types/enum/ButtonEnum";
+import Title from "../../title/Title";
+
+import { FaGithub } from "react-icons/fa6";
+import { RiNotionFill } from "react-icons/ri";
+import LabelInput from "../../inputs/labelInput/LabelInput";
+import ErrorToolTip from "../../tooltip/ErrorToolTip";
+
+interface ContactList {
+  path: string;
+  icon: JSX.Element;
+  title: string;
+}
 
 const Contact = () => {
   const formRef = useRef<HTMLFormElement>(null);
 
-  const contactMutation = useMutation<
-    unknown,
-    Error,
-    FormEvent<HTMLFormElement>
-  >({
-    mutationFn: (e) => FetchContact(e, formRef),
-    onSuccess: () => {
-      console.log("success");
-      toast.success("작성하신 내용을 메일로 전달하였습니다.");
+  const contactList: ContactList[] = [
+    {
+      path: "https://github.com/gustn1029?tab=repositories",
+      icon: <FaGithub />,
+      title: "GitHub",
     },
-    onError: (error) => {
-      toast.error(error.message);
+    {
+      path: "https://www.notion.so/128bd9caac2a804c8212cd287e7a2551?pvs=12",
+      icon: <RiNotionFill />,
+      title: "Notion",
     },
-  });
+  ];
 
   const {
     register,
@@ -38,8 +47,21 @@ const Contact = () => {
     setValue,
   } = useForm<ContactMailData>();
 
-  const onSubmit = async () => {
-    reset();
+  const contactMutation = useMutation<unknown, Error, ContactMailData>({
+    mutationFn: () => FetchContact(formRef),
+    onSuccess: () => {
+      console.log("success");
+      toast.success("작성하신 내용을 메일로 전달하였습니다.");
+      reset();
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error(error.message);
+    },
+  });
+
+  const onSubmit = async (data: ContactMailData) => {
+    await contactMutation.mutateAsync(data);
   };
 
   // 이메일 정규식
@@ -47,52 +69,87 @@ const Contact = () => {
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return (
     <SectionWrap id="contact">
-      <form ref={formRef} onSubmit={handleSubmit(onSubmit)}>
-        <LabelLayout inputId="from_mail" errors={errors} label="이메일">
-          <input
+      <Title title="Contact" desc="많은 관심 부탁드립니다." />
+      <div className="flex gap-[60px] justify-between items-center">
+        <ul className="w-[calc(50%-30px)] flex gap-[30px] items-center justify-center">
+          {contactList.map((contact) => (
+            <li
+              key={`contact__${contact.title}`}
+              className={`${
+                contact.title === "GitHub" ? "text-[4rem]" : "text-[5rem]"
+              } text-gray-400 hover:text-white transition-color`}
+            >
+              <a href={contact.path} target="_blank">
+                {contact.icon}
+                <span className="sr-only">{contact.title}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-[calc(50%-30px)] flex flex-col gap-[40px] bg-[#2d3038] p-[30px] rounded-[30px]"
+        >
+          <LabelInput
             type="email"
-            id="from_mail"
-            {...register("from_email", {
+            register={register("from_name", {
               required: {
                 value: true,
-                message: "이메일 주소를 입력해 주세요.",
+                message: "이메일 주소 입력은 필수입니다.",
               },
               pattern: {
                 value: rfc5322EmailRegex,
                 message: "올바른 이메일 형식이 아닙니다.",
               },
             })}
+            inputId="from_name"
+            errors={errors.from_name}
+            label="이메일"
+            placeholder="이메일을 입력해 주세요."
           />
-        </LabelLayout>
-        <LabelLayout inputId="message" errors={errors} label="내용">
-          <textarea
-            id="message"
-            {...register("message", {
-              required: {
-                value: true,
-                message: "전달하실 내용을 입력해 주세요.",
-              },
-              maxLength: {
-                value: 1000,
-                message: "내용은 최대 1000자까지 입력 가능합니다.",
-              },
-              onChange: (e: ChangeEvent<HTMLTextAreaElement>) => {
-                const value = e.target.value;
-                if (value.length > 1000) {
-                  setValue("message", value.slice(0, 1000));
-                }
-              },
-            })}
-          ></textarea>
-        </LabelLayout>
-        <input
-          className={`block py-[10px] px-[20px] text-white ${
-            ButtonStyleList[ButtonStyleEnum.PRIMARY].value
-          } rounded-[10px]`}
-          type="submit"
-          value="전송하기"
-        />
-      </form>
+          <LabelInput
+            register={register("phone_number")}
+            inputId="phone_number"
+            errors={errors.phone_number}
+            label="전화번호"
+            placeholder="하이픈(-)을 포함한 전화번호를 입력해 주세요."
+          />
+          <LabelLayout inputId="message" label="내용">
+            <textarea
+              className="rounded-[20px] resize-none min-h-[200px] p-[20px]"
+              placeholder="내용을 입력해 주세요."
+              id="message"
+              {...register("message", {
+                required: {
+                  value: true,
+                  message: "전달하실 내용을 입력해 주세요.",
+                },
+                maxLength: {
+                  value: 1000,
+                  message: "내용은 최대 1000자까지 입력 가능합니다.",
+                },
+                onChange: (e: ChangeEvent<HTMLTextAreaElement>) => {
+                  const value = e.target.value;
+                  if (value.length > 1000) {
+                    setValue("message", value.slice(0, 1000));
+                  }
+                },
+              })}
+            ></textarea>
+            {errors && errors.message && errors.message.message && (
+              <ErrorToolTip message={errors.message.message} />
+            )}
+          </LabelLayout>
+          <input
+            className={`block py-[10px] px-[20px] text-white cursor-pointer ${
+              ButtonStyleList[ButtonStyleEnum.PRIMARY].value
+            } rounded-[10px]`}
+            type="submit"
+            value="전송하기"
+          />
+        </form>
+      </div>
     </SectionWrap>
   );
 };
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,26 +1,25 @@
 import emailjs from "@emailjs/browser";
 import { QueryClient } from "@tanstack/react-query";
-import { FormEvent } from "react";
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient();
 
-export const FetchContact = async (e:FormEvent<HTMLFormElement>, formRef: React.RefObject<HTMLFormElement>) => {
-    if (!formRef.current) throw new Error("Form reference is null");
-    
-    e.preventDefault();
+export const FetchContact = async (
+  formRef: React.RefObject<HTMLFormElement>
+) => {
+  if (!formRef.current) throw new Error("Form reference is null");
 
-    try {
-        const result = await emailjs.sendForm(
-            "YOUR_SERVICE_ID", 
-            "YOUR_TEMPLATE_ID", 
-            formRef.current, 
-            {
-                publicKey: "YOUR_PUBLIC_KEY",
-            }
-        );
-        return result;
-    } catch (error) {
-        console.error(error);
-        throw new Error("메일 발송 중 에러가 발생했습니다.");
-    }
+  try {
+    const result = await emailjs.sendForm(
+      import.meta.env.VITE_MAIL_ID,
+      import.meta.env.VITE_MAIL_TEMPLET_ID,
+      formRef.current,
+      {
+        publicKey: import.meta.env.VITE_MAIL_KEY,
+      }
+    );
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw new Error("메일 발송 중 에러가 발생했습니다.");
+  }
 };
