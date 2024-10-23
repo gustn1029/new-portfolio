b0bfaae9ad5636f99763d6179df5174f23e16522
hs0820 <gustn1029@gmail.com>
2024-10-23 23:45:47 +0900

    Contact, Label 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,30 @@
+import { ReactNode } from "react";
+import { FieldErrors } from "react-hook-form";
+
+export interface CareerItem {
+  period: string;
+  company?: string;
+  education?: string;
+  desc: string[];
+}
+
 export interface ProjectOption {
-    title: string;
-    path: string;
-    image: string;
-    skills: string[];
-    desc: string;
-    projectInfo: string[];
-  }
+  title: string;
+  path: string;
+  image: string;
+  skills: string[];
+  desc: string;
+  projectInfo: string[];
+}
+
+export interface ContactMailData {
+  from_email: string;
+  message: string;
+}
+
+export interface LabelLayoutProps {
+  label: string;
+  children: ReactNode;
+  errors: FieldErrors<ContactMailData>;
+  inputId: string;
+}
