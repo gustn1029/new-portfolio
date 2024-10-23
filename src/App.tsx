8243a2e354a8449239884475e925c2ff86e50410
hs0820 <gustn1029@gmail.com>
2024-10-24 01:19:06 +0900

    Toaster 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./pages/main/Main";
 import RootLayout from "./components/layouts/RootLayout";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const router = createBrowserRouter([
@@ -10,7 +11,12 @@ function App() {
       children: [{ path: "/", element: <Main /> }],
     },
   ]);
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster />
+    </>
+  );
 }
 
 export default App;
