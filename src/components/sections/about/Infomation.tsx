414678bb92b52b283d0cbf0151915ef7cbf2cc5c
hs0820 <gustn1029@gmail.com>
2024-10-24 16:11:36 +0900

    소개 글 수정

--- src/components/sections/about/Infomation.tsx
+++ src/components/sections/about/Infomation.tsx
@@ -14,16 +14,20 @@ const Infomation = ({ infomationData }: InfomationProps) => {
       <p className="text-white text-[1.3rem] mb-[20px]">
         안녕하세요
         <br />
-        <em className="text-point">기린 같은 주니어 프론트엔드 개발자</em>{" "}
+        <em className="text-point">
+          책임감 넘치는 주니어 프론트엔드 개발자
+        </em>{" "}
         김현수입니다.
       </p>
-      <p className="text-gray-400 mb-[30px]">
-        <em className="text-gray-200">기린</em>은 세상에서 가장 긴 목을 가진
-        동물로, 멀리 있는 것들을 바라보고{" "}
-        <em className="text-gray-200">넓은 시야</em>를 가질 수 있습니다.
+      <p className="text-gray-400 mb-[30px] break-keep">
+        저는
+        <em className="text-gray-200"> 기한 준수</em>를 핵심 가치로 삼고,
+        <em className="text-gray-200"> 코드 품질</em>을 조화시키는 개발자입니다.
         <br />
-        이처럼 저는 <em className="text-gray-200">항상 더 넓은 시야</em>를 가지려고 노력하며, <em className="text-gray-200">새로운 기술과
-        트렌드를 빠르게 파악해 나가는 개발자</em>가 되고자 합니다.
+        <em className="text-gray-200">항상 더 넓은 시야</em>를 가지려고
+        노력하며,{" "}
+        <em className="text-gray-200">새로운 기술과 트렌드를 빠르게 파악</em>해
+        나가고, 품질과 책임을 다하는 개발자가 되겠습니다.
       </p>
       <ul className="flex flex-col gap-[10px]">
         {infomationData.map((info) => (
